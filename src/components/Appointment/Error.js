554b1474d8a8afbd8cc73723f5bf0973d2156944
Nael Hubaishi <nael.hub@gmail.com>
2022-03-25 16:12:18 -0400

    updates

--- src/components/Appointment/Error.js
+++ src/components/Appointment/Error.js
@@ -1,17 +1,32 @@
 import React from 'react'
 
 
-<main className="appointment__card appointment__card--error">
-  <section className="appointment__error-message">
-    <h1 className="text--semi-bold">Error</h1>
-    <h3 className="text--light">{props.message}</h3>
-  </section>
-  <img
-    className="appointment__error-close"
-    src="images/close.png"
-    alt="Close"
-    onClick={props.onClose}
-  />
-</main>
+export default function Show(props) {
+  return (
+    <main className="appointment__card appointment__card--show">
+      <section className="appointment__card-left">
+        <h2 className="text--regular">{props.student}</h2>
+        <section className="interviewer">
+          <h4 className="text--light">Interviewer</h4>
+          <h3 className="text--regular">{props.interviewer.name}</h3>
+        </section>
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <img
+            className="appointment__actions-button"
+            src="images/edit.png"
+            alt="Edit"
+            onClick={props.onEdit}
+          />
+          <img
+            className="appointment__actions-button"
+            src="images/trash.png"
+            alt="Delete"
+            onClick={() => props.onDelete(props.interview)}
+          />
+        </section>
+      </section>
+    </main>
   )
 }
